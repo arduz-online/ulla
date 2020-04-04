7a2bad1b4ff8118582573f61d02300cb4d735c9e
Agustin Mendez <agustin.mz.92@gmail.com>
2020-04-04 17:04:22 -0300

    add missing test file

---
+++ packages/ulla-amd/test/simple.spec.js
@@ -0,0 +1,160 @@
+const expect = require("expect");
+
+describe("simple test with external module", () => {
+  const starters = [];
+  it("defines global.*", () => {
+    global.loadModule = function(moduleName) {
+      if (moduleName == "@dcl/test")
+        return Promise.resolve({
+          rpcHandle: "123",
+          methods: [
+            {
+              name: "xxx"
+            },
+            {
+              name: "zzz"
+            }
+          ]
+        });
+      throw new Error("Unknown module " + moduleName);
+    };
+
+    global.callRpc = function(moduleHandle, methodName, args) {
+      if (moduleHandle !== "123") {
+        throw new Error("Unknown module handle " + moduleHandle);
+      }
+
+      if (methodName === "xxx") {
+        return Promise.resolve(args.reduce((a, b) => a + b, 0));
+      }
+
+      return Promise.reject(new Error("Unknown method"));
+    };
+
+    global.onStart = function(cb) {
+      starters.push(cb);
+    };
+
+    var name = require.resolve("../dist/amd");
+    delete require.cache[name];
+    require(name);
+  });
+
+  it("defines a module that loads other module that loads @dcl/test", done => {
+    global.define("test", ["a"], a => {
+      try {
+        expect(a.exportedTestDCL).toHaveProperty("xxx");
+        expect(a.exportedTestDCL).toHaveProperty("zzz");
+      } catch (e) {
+        done(e);
+        return;
+      }
+
+      a.exportedTestDCL
+        .xxx(1, 2, 3, 4)
+        .then(r => {
+          expect(r).toEqual(10);
+
+          a.exportedTestDCL
+            .zzz()
+            .then(r => {
+              done("didnt fail");
+            })
+            .catch(() => done());
+        })
+        .catch(done);
+    });
+
+    define("a", ["exports", "@dcl/test"], (exports, testDCL) => {
+      exports.exportedTestDCL = testDCL;
+    });
+  });
+
+  it("starters must not throw", () => {
+    expect(starters.length).toBeGreaterThan(0);
+    starters.forEach($ => $());
+  });
+});
+
+describe("simple test with external module that doesnt exist and throw", () => {
+  const starters = [];
+  it("defines global.*", () => {
+    global.loadModule = function loadModule(moduleName) {
+      if (moduleName == "@throw/test")
+        return Promise.resolve({
+          rpcHandle: "123",
+          methods: [
+            {
+              name: "xxx"
+            },
+            {
+              name: "zzz"
+            }
+          ]
+        });
+      throw new Error("Unknown module " + moduleName);
+    };
+
+    global.callRpc = function(moduleHandle, methodName, args) {
+      if (moduleHandle !== "123") {
+        throw new Error("Unknown module handle " + moduleHandle);
+      }
+
+      if (methodName === "xxx") {
+        return Promise.resolve(args.reduce((a, b) => a + b, 0));
+      }
+
+      return Promise.reject(new Error("Unknown method"));
+    };
+
+    global.onStart = function(cb) {
+      starters.push(cb);
+    };
+
+    var name = require.resolve("../dist/amd");
+    delete require.cache[name];
+    require(name);
+  });
+
+  it("defines a module that loads other module that loads @throw/test", done => {
+    global.define("test", ["a"], a => {
+      try {
+        expect(a.exportedTestDCL).toHaveProperty("xxx");
+        expect(a.exportedTestDCL).toHaveProperty("zzz");
+      } catch (e) {
+        done(e);
+        return;
+      }
+
+      a.exportedTestDCL
+        .xxx(1, 2, 3, 4)
+        .then(r => {
+          expect(r).toEqual(10);
+
+          a.exportedTestDCL
+            .zzz()
+            .then(r => {
+              done("didnt fail");
+            })
+            .catch(() => done());
+        })
+        .catch(done);
+    });
+
+    expect(() =>
+      define("a", ["exports", "@throw/test", "@throw/test2", "@throw/tes3"], (
+        exports,
+        testDCL
+      ) => {
+        exports.exportedTestDCL = testDCL;
+      })
+    ).toThrow();
+
+    done();
+  });
+
+  it("starters must not throw", () => {
+    expect(starters.length).toBeGreaterThan(0);
+    expect(() => starters.forEach($ => $())).toThrow();
+  });
+});
