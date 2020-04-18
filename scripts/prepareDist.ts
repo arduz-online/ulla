58801d74dae01080e1da674dd717ca7f2af4dbbf
Agustin Mendez <agustin.mz.92@gmail.com>
2020-04-18 20:03:15 -0300

    ulla-builder is no longer embeded in ulla-ecs

--- scripts/prepareDist.ts
+++ scripts/prepareDist.ts
@@ -7,14 +7,13 @@ import { copyFile } from "./_utils";
 
 const root = path.resolve(__dirname, "..");
 
-
 async function injectDependencies(
   folder: string,
   dependencies: string[],
   devDependency = false
 ) {
   console.log(
-    `> update ${folder}/package.json (injecting dependencies & version)`
+    `> update ${folder}/package.json (copying dependencies & version)`
   );
   {
     const file = path.resolve(root, `${folder}/package.json`);
@@ -29,7 +28,7 @@ async function injectDependencies(
 
     packageJson[target] = packageJson[target] || {};
 
-    deps.forEach(dep => {
+    deps.forEach((dep) => {
       if (localPackageJson.dependencies[dep]) {
         packageJson[target][dep] = localPackageJson.dependencies[dep];
         deps.delete(dep);
@@ -37,7 +36,7 @@ async function injectDependencies(
       }
     });
 
-    deps.forEach(dep => {
+    deps.forEach((dep) => {
       if (localPackageJson.devDependencies[dep]) {
         packageJson[target][dep] = localPackageJson.devDependencies[dep];
         deps.delete(dep);
@@ -61,19 +60,42 @@ async function injectDependencies(
   }
 }
 
+// async function injectDependencyVersion(
+//   folder: string,
+//   dependencies: Record<string, string>,
+//   devDependency = false
+// ) {
+//   console.log(
+//     `> update ${folder}/package.json (injecting dependencies & version)`
+//   );
+//   {
+//     const file = path.resolve(root, `${folder}/package.json`);
+//     const packageJson = JSON.parse(readFileSync(file).toString());
+
+//     const deps = Object.entries(dependencies);
+
+//     const target = devDependency ? "devDependencies" : "dependencies";
+
+//     packageJson[target] = packageJson[target] || {};
+
+//     deps.forEach(([dep, version]) => {
+//         packageJson[target][dep] = version;
+//         console.log(`  using dependency: ${dep}@${packageJson[target][dep]}`);
+//     });
+
+//     writeFileSync(file, JSON.stringify(packageJson, null, 2));
+//   }
+// }
+
 async function prepareECS(folder: string) {
   copyFile(
     path.resolve(root, `packages/ulla-amd/dist/amd.js`),
     path.resolve(root, `${folder}/artifacts/amd.js`)
   );
-  copyFile(
-    path.resolve(root, `packages/ulla-builder/index.js`),
-    path.resolve(root, `${folder}/artifacts/ulla-builder.js`)
-  );
 }
 
 // tslint:disable-next-line:semicolon
-(async function() {
+(async function () {
   await prepareECS("packages/ulla-ecs");
   await copyFile("LICENSE", "packages/ulla-ecs/LICENSE");
   await copyFile("NOTICE", "packages/ulla-ecs/NOTICE");
@@ -84,18 +106,14 @@ async function prepareECS(folder: string) {
   await copyFile("LICENSE", "packages/ulla-builder/LICENSE");
   await copyFile("NOTICE", "packages/ulla-builder/NOTICE");
 
-  await injectDependencies(
-    "packages/ulla-ecs",
-    ["typescript", "terser", "isolated-vm"],
-    false
-  );
+  await injectDependencies("packages/ulla-ecs", [], false);
   await injectDependencies(
     "packages/ulla-builder",
     ["typescript", "terser", "isolated-vm"],
     false
   );
   await injectDependencies("packages/ulla-compiler", ["typescript"], false);
-})().catch(e => {
+})().catch((e) => {
   // tslint:disable-next-line:no-console
   console.error(e);
   process.exit(1);
